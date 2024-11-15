f4ce7ba50118e89f230e22f8a262bc8c36a89c1c
ratman27 <144323355+ratman27@users.noreply.github.com>
2024-11-15 18:39:48 +0530

    Create predictions.py

---
+++ predictions.py
@@ -0,0 +1,6 @@
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1    
+    W2 = W2 - alpha * dW2  
+    b2 = b2 - alpha * db2    
+    return W1, b1, W2, b2
